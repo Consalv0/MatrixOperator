bbe7036b79ee17e03fe885460ad04c5264c8c2ae
Consalvo <gonzaloceciliano@gmail.com>
2017-04-19 03:49:52 -0500

    Buttons

--- js/main.js
+++ js/main.js
@@ -1,46 +1,80 @@
 /* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0, no-trailing-spaces: 0, no-irregular-whitespace: 0 */
 /* global $ */
 
-/* BUTTONS */
-function addPlus(path) {
-  let button = '<button class="btn btn-success">⏎'
-  $(path).append(button)
-}
-
-function addSub() {
-  let button = '<button class="btn btn-danger">-'
-  $('body #matrix').append(button)
-}
-
-function addTable(type) {
-  let row = '<div class="row">'
-  let path = 'body .container#matrixTable' + type
+/* TABLE */
+function addRow(path, num) {
+  let row = '<div class="d-flex flex-nowrap" id="' + num + '">'
   $(path).append(row)
-  addCell(path + ' .row')
-  addCell(path + ' .row')
 }
 
-function addCell(path, n, m, value) {
-  let text = '<input type="text" class="form-control" id="' + n + '-' + m + '" placeholder="' + value + '" style="text-align:right;">'
+function addCell(path, value) {
+  let text = '<input type="text" class="form-control" placeholder="' + value + '" style="text-align:right;">'
   $(path).append(text)
 }
 /* ******* */
 
 /* MAKE */
-// addTable('A')
-// addTable('B')
-let dataA = [[[0, 0], 1], [[0, 1], 2], [[1, 0], 3], [[1, 1], 4]]
-let dataB = [[[0, 0], 1], [[0, 1], 2], [[1, 0], 3], [[1, 1], 4]]
-
-function table(data, id) {
-  data.forEach(function(element) {
-    let n = element[0][0]
-    let m = element[0][1]
-    let value = element[1]
-    addCell('body .container#mTable' + id, n, m, value)
-  })
-  addPlus('body .container#mTable' + id)
+function doMatrix(matrix, rows, columns) {
+  let newRow = []
+  for (let j = 0; j < columns; j++) {
+    newRow.push(0)
+  }
+  for (let i = 0; i < rows; i++) {
+    matrix.push(newRow)
+  }
 }
-table(dataA, 'A')
-table(dataB, 'B')
+
+function makeTable(matrix, name) {
+  matrix.forEach(function (matrix, i) {
+    addRow('#mTable' + name + ' #table', i)
+    matrix.forEach(function (matrix) {
+      addCell('#mTable' + name + ' #table' + ' #' + i, matrix)
+    })
+  })
+}
+
+var matrixA = []
+var matrixB = []
+doMatrix(matrixA, 5, 3)
+doMatrix(matrixB, 2, 3)
+makeTable(matrixA, 'A')
+makeTable(matrixB, 'B')
 /* ******* */
+
+function getMatrixWithID(parentID) {
+  parentID = parentID.toString()
+  parentID = parentID.replace(/mTable/g, '')
+  if (parentID === 'A') return matrixA
+  if (parentID === 'B') return matrixB
+}
+
+$('#columns #sum').click(function(matrix) {
+  let parentID = $(this).parent().parent().parent().attr('id')
+  matrix = getMatrixWithID(parentID)
+  addRow('#' + parentID + ' #table', matrix.length)
+  let newRow = []
+  matrix[0].forEach(function () {
+    addCell('#' + parentID + ' #table #' + matrix.length, 0)
+    newRow.push(0)
+  })
+  matrix.push(newRow)
+})
+
+$('#columns #sub').click(function(matrix) {
+  let parentID = $(this).parent().parent().parent().attr('id')
+  matrix = getMatrixWithID(parentID)
+  if (matrix.length === 1) return
+  $('#' + parentID + ' #table #' + (matrix.length-1)).remove()
+  matrix.pop()
+})
+
+$('#rows #sum').click(function(matrix) {
+  let parentID = $(this).parent().parent().parent().attr('id')
+  matrix = getMatrixWithID(parentID)
+  $('#' + parentID + ' #table div').each(function(idx) {
+    addCell('#' + parentID + ' #table #' + idx, 0)
+  })
+  matrix.forEach(function (mtx, idx) {
+    mtx.push(0)
+  })
+})
