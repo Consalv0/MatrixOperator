eafb78d5aa5c0992cd927e0d8ec792d178b9c674
Consalvo <gonzaloceciliano@gmail.com>
2017-05-10 05:30:09 -0500

    Added System of Ecuations Solver

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 /* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0, no-trailing-spaces: 0, no-irregular-whitespace: 0, no-eval: 0 */
 /* global $ */
 
-/* TABLE */
 function addRow(path, id, row) {
   row = '<div class="d-flex flex-nowrap" id="' + id + '">'
   $(path).append(row)
@@ -20,17 +19,19 @@ function addColumn(path, id, value, column) {
   }
   $(path).append(column)
 }
-/* ******* */
 
-function printedNumber(x) {
-  if (!x && x !== 0) return '∞'
-  Number(Math.round(x+'e2')+'e-2')
+function printedNumber(x, commas = true) {
+  if (!x && x !== 0 || x === Infinity || x === -Infinity) return '∞'
+  x = Number(Math.round(x+'e2')+'e-2')
   let parts = x.toString().split('.')
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-  return parts.join('.')
+  if (commas) {
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    return parts.join('.')
+  } else {
+    return x
+  }
 }
 
-/* MAKE */
 function doMatrix(matrix, rows, columns) {
   for (let i = 0; i < rows; i++) {
     matrix.push([])
@@ -42,14 +43,15 @@ function doMatrix(matrix, rows, columns) {
 }
 
 function makeTable(matrix, name) {
-  $('#mTable' + name + ' table').empty()
+  $('#mTable' + name + ' table#mtrx').empty()
   matrix.forEach(function (matrix, i) {
-    addRow('#mTable' + name + ' table', i)
+    addRow('#mTable' + name + ' table#mtrx', i)
     matrix.forEach(function (matrix, j) {
-      addColumn('#mTable' + name + ' table' + ' div#' + i, j, matrix)
+      addColumn('#mTable' + name + ' table#mtrx' + ' div#' + i, j, matrix)
     })
   })
   sizeText(matrix, '#mTable' + name)
+  toggleSolveAxis(matrix, name)
 }
 
 var matrixA = []
@@ -60,6 +62,35 @@ doMatrix(matrixB, 3, 3)
 makeTable(matrixA, 'A')
 makeTable(matrixB, 'B')
 /* ******* */
+
+function matrixSizeIs(matrix, rows = 0, condRow = '>', columns = 0, condCol = '>') {
+  if (matrix[0].length >= rows && condRow === '>') {
+    if (matrix.length >= columns && condCol === '>') return true
+    if (matrix.length <= columns && condCol === '<') return true
+    if (matrix.length === columns && condCol === '=') return true
+  }
+  if (matrix[0].length <= rows && condRow === '<') {
+    if (matrix.length >= columns && condCol === '>') return true
+    if (matrix.length <= columns && condCol === '<') return true
+    if (matrix.length === columns && condCol === '=') return true
+  }
+  if (matrix[0].length === rows && condRow === '=') {
+    if (matrix.length >= columns && condCol === '>') return true
+    if (matrix.length <= columns && condCol === '<') return true
+    if (matrix.length === columns && condCol === '=') return true
+  }
+  return false
+}
+
+function toggleSolveAxis(matrix, id) {
+  id = getMatrixWithID(id, 'onlyID')
+  if (matrixSizeIs(matrix, 4, '=', 3, '=')) {
+    $('#mTable' + id + ' #axisSolved').removeClass('hidden-xs-up')
+    solveAxis(id)
+  } else {
+    $('#mTable' + id + ' #axisSolved').addClass('hidden-xs-up')
+  }
+}
 
 function getMatrixWithID(parentID, onlyID = false) {
   parentID = parentID.toString()
@@ -209,6 +240,62 @@ function doDeterminant(matrix, mSolved = []) {
   return mSolved
 }
 
+function solveAxis(id) {
+  let matrix = getMatrixWithID(id)
+
+  let matrixS = []
+  for (let i = 0; i < matrix.length; i++) {
+    matrixS.push([])
+    for (let j = 0; j < matrix[i].length -1; j++) {
+      matrixS[i].push(matrix[i][j])
+    }
+  }
+  let deltaS = doDeterminant(matrixS)
+  $('#mTable' + id + ' #axisSolved var.s').text('∆𝑠 = ' + printedNumber(deltaS, false))
+
+  let matrixX = []
+  for (let i = 0; i < matrix.length; i++) {
+    matrixX.push([])
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (j === matrix[i].length -1) {
+        matrixX[i][0] = matrix[i][j]
+      } else {
+        matrixX[i].push(matrix[i][j])
+      }
+    }
+  }
+  let deltaX = doDeterminant(matrixX)
+  $('#mTable' + id + ' #axisSolved var.x').text('𝑥 = ' + printedNumber(deltaX / deltaS, false))
+
+  let matrixY = []
+  for (let i = 0; i < matrix.length; i++) {
+    matrixY.push([])
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (j === matrix[i].length -1) {
+        matrixY[i][1] = matrix[i][j]
+      } else {
+        matrixY[i].push(matrix[i][j])
+      }
+    }
+  }
+  let deltaY = doDeterminant(matrixY)
+  $('#mTable' + id + ' #axisSolved var.y').text('𝑦 = ' + printedNumber(deltaY / deltaS, false))
+
+  let matrixZ = []
+  for (let i = 0; i < matrix.length; i++) {
+    matrixZ.push([])
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (j === matrix[i].length -1) {
+        matrixZ[i][2] = matrix[i][j]
+      } else {
+        matrixZ[i].push(matrix[i][j])
+      }
+    }
+  }
+  let deltaZ = doDeterminant(matrixZ)
+  $('#mTable' + id + ' #axisSolved var.z').text('𝑧 = ' + printedNumber(deltaZ / deltaS, false))
+}
+
 $(document).on('click', '#tras', function () {
   let parentID = $(this).parents('.card').attr('id')
   let id = getMatrixWithID(parentID, 'onlyID')
@@ -224,6 +311,7 @@ $(document).on('click', '#multBy', function () {
           ? $(this).parents('.input-group').find('input').val() : 1
   num = eval(`(${num})`)
   doMultBy(id, num)
+  toggleSolveAxis(getMatrixWithID(id), id)
   if (id === 'C') { unCheck('#matrixOperator'); return }
   doOperation()
 })
@@ -268,7 +356,12 @@ $(document).on('click', '#matrixOperator label', function () {
   doOperation()
 })
 
-$(document).on('select change', '#matrixOperator select', doOperation)
+$(document).on('select change', '#matrixOperator select', function() {
+  let parentID = $(this).parents('.card').attr('id')
+  let matrix = getMatrixWithID(parentID)
+  doOperation()
+  toggleSolveAxis(matrix, parentID)
+})
 
 $(document).on('change keyup', '.matrixListener', function() {
   let parentID = $(this).parents('.card').attr('id')
@@ -279,54 +372,59 @@ $(document).on('change keyup', '.matrixListener', function() {
   matrix[column][row] = value
   doOperation()
   sizeText(matrix, '#' + parentID)
+  toggleSolveAxis(matrix, parentID)
 })
 
 $(document).on('click', '#rows #sum', function(matrix) {
   let parentID = $(this).parents('.card').attr('id')
   matrix = getMatrixWithID(parentID)
   if (matrix.length === 12) return
-  addRow('#' + parentID + ' table', matrix.length)
+  addRow('#' + parentID + ' table#mtrx', matrix.length)
   let newRow = []
   matrix[0].forEach(function (element, idx) {
-    addColumn('#' + parentID + ' table div#' + matrix.length, idx)
+    addColumn('#' + parentID + ' table#mtrx div#' + matrix.length, idx)
     newRow.push(0)
   })
   matrix.push(newRow)
   sizeText(matrix, '#' + parentID)
   doOperation()
+  toggleSolveAxis(matrix, parentID)
 })
 
 $(document).on('click', '#rows #sub', function(matrix) {
   let parentID = $(this).parents('.card').attr('id')
   matrix = getMatrixWithID(parentID)
   if (matrix.length === 1) return
-  $('#' + parentID + ' table div#' + (matrix.length-1)).fadeOut(100, function() {
+  $('#' + parentID + ' table#mtrx div#' + (matrix.length-1)).fadeOut(100, function() {
     $(this).remove()
   })
   matrix.pop()
   sizeText(matrix, '#' + parentID)
   doOperation()
+  toggleSolveAxis(matrix, parentID)
 })
 
 $(document).on('click', '#columns #sum', function(matrix) {
   let parentID = $(this).parents('.card').attr('id')
   matrix = getMatrixWithID(parentID)
   if (matrix[0].length === 12) return
-  $('#' + parentID + ' table div').each(function(idx) {
-    addColumn('#' + parentID + ' table div#' + idx, matrix[0].length)
+  $('#' + parentID + ' table#mtrx div').each(function(idx) {
+    addColumn('#' + parentID + ' table#mtrx div#' + idx, matrix[0].length)
   })
   matrix.forEach(function (mtx) {
     mtx.push(0)
   })
   sizeText(matrix, '#' + parentID)
   doOperation()
+  parentID = parentID.replace(/mTable/g, '')
+  toggleSolveAxis(matrix, parentID)
 })
 
 $(document).on('click', '#columns #sub', function(matrix) {
   let parentID = $(this).parents('.card').attr('id')
   matrix = getMatrixWithID(parentID)
   if (matrix[0].length === 1) return
-  $('#' + parentID + ' table div').each(function(idx, element) {
+  $('#' + parentID + ' table#mtrx div').each(function(idx, element) {
     $(element).children().last().fadeOut(100, function() {
       $(this).remove()
     })
@@ -336,6 +434,8 @@ $(document).on('click', '#columns #sub', function(matrix) {
   })
   sizeText(matrix, '#' + parentID)
   doOperation()
+  parentID = parentID.replace(/mTable/g, '')
+  toggleSolveAxis(matrix, parentID)
 })
 
 doOperation()
